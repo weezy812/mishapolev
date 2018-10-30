48b5328a8b14127f10ebea7a8a286b08c6be027e
Michael Polev <44271317+weezy812@users.noreply.github.com>
2018-10-30 17:30:11 +0200

    Add files via upload

---
+++ lesson 2 h.w./lesson2hw.js
@@ -0,0 +1,25 @@
+//Homework:learn.javascript.ru
+
+//2.1
+alert('Я-Javascript!');
+//2.5
+var admin;
+var name;
+var name = 'Василий';
+admin = name;
+console.log(admin);
+//2.6
+var planet = 'Earth';
+var name = 'Петя';
+
+//2.8
+var a = 1, b = 1, c, d;
+
+c = ++a; alert(c); // 2
+d = b++; alert(d); // 1
+
+c = (2+ ++a); alert(c); // 5
+d = (2+ b++); alert(d); // 4
+
+alert(a); // 3
+alert(b); // 3
